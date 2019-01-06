74f52307e6ae74c7fdd4e90086d29f0e93ad1977
moussetc <moussetc@users.noreply.github.com>
2019-01-06 21:12:35 +0100

    Handle parsing 'D8' as '1D8' and lowercase input + add parsing tests

--- src/io/read.rs
+++ src/io/read.rs
@@ -7,10 +7,11 @@ impl FromStr for DiceRequest {
     type Err = Error;
     // Parse a string to find a dice definition (number of dice + dice type + optional dice parameters)
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.ends_with("F") {
-            let number_from_str = match s.len() {
+        let input = s.trim().to_uppercase();
+        if input.ends_with("F") {
+            let number_from_str = match input.len() {
                 1 => 1,
-                _ => s[0..s.len() - 1].parse::<DiceNumber>()?,
+                _ => input[0..input.len() - 1].parse::<DiceNumber>()?,
             };
             return Ok(DiceRequest {
                 kind: DiceKind::TextKind(TextDice::FudgeDice(FudgeDice::new())),
@@ -18,8 +19,8 @@ impl FromStr for DiceRequest {
             });
         }
 
-        if s.starts_with("+") {
-            let number_from_str = s[..s.len()].parse::<NumericRoll>()?;
+        if input.starts_with("+") {
+            let number_from_str = input[..input.len()].parse::<NumericRoll>()?;
             return Ok(DiceRequest {
                 kind: DiceKind::NumericKind(NumericDice::ConstDice(ConstDice::new(
                     number_from_str,
@@ -28,13 +29,16 @@ impl FromStr for DiceRequest {
             });
         }
 
-        if s.contains("D") {
-            let parts: Vec<&str> = s.trim().split('D').collect();
+        if input.contains("D") {
+            let parts: Vec<&str> = input.split('D').collect();
 
             match parts.len() {
                 1 | 2 => {
                     // Format: D6 or 3D6
-                    let mut number_fromstr = parts[0].parse::<u8>()?;
+                    let mut number_fromstr = match parts[0].len() {
+                        0 => 1,
+                        _ => parts[0].parse::<u8>()?,
+                    };
                     let sides_fromstr = parts[parts.len() - 1].parse::<u16>()?;
                     // the number of dice to roll is optional and defaults to 1
                     if parts.len() == 1 {
@@ -73,4 +77,64 @@ impl FromStr for RollRequest {
 
         Ok(RollRequest::new(parts))
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::dice::*;
+    use crate::DiceRequest;
+    use std::str::FromStr;
+
+    #[test]
+    fn read_numbered_dice() {
+        assert_eq!(
+            DiceRequest::from_str(&String::from("5d6")).unwrap(),
+            DiceRequest::new(
+                DiceKind::NumericKind(NumericDice::NumberedDice(NumberedDice::new(6))),
+                5
+            )
+        );
+
+        assert_eq!(
+            DiceRequest::from_str(&String::from("8D3")).unwrap(),
+            DiceRequest::new(
+                DiceKind::NumericKind(NumericDice::NumberedDice(NumberedDice::new(3))),
+                8
+            )
+        );
+
+        assert_eq!(
+            DiceRequest::from_str(&String::from("D20")).unwrap(),
+            DiceRequest::new(
+                DiceKind::NumericKind(NumericDice::NumberedDice(NumberedDice::new(20))),
+                1
+            )
+        );
+    }
+
+    #[test]
+    fn read_fudge_dice() {
+        assert_eq!(
+            DiceRequest::from_str(&String::from("F")).unwrap(),
+            DiceRequest::new(DiceKind::TextKind(TextDice::FudgeDice(FudgeDice::new())), 1)
+        );
+
+        assert_eq!(
+            DiceRequest::from_str(&String::from("8F")).unwrap(),
+            DiceRequest::new(DiceKind::TextKind(TextDice::FudgeDice(FudgeDice::new())), 8)
+        );
+    }
+
+    #[test]
+    fn read_ko() {
+        DiceRequest::from_str(&String::from("5")).unwrap_err();
+        DiceRequest::from_str(&String::from("Da")).unwrap_err();
+        DiceRequest::from_str(&String::from("D8D")).unwrap_err();
+        DiceRequest::from_str(&String::from("F8")).unwrap_err();
+        DiceRequest::from_str(&String::from("+")).unwrap_err();
+        DiceRequest::from_str(&String::from("8+")).unwrap_err();
+        DiceRequest::from_str(&String::from("+8+")).unwrap_err();
+        DiceRequest::from_str(&String::from("2+8")).unwrap_err();
+    }
+
 }
